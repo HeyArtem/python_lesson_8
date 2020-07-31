64b1184035fb2d2f254bf6e5585559e25fb7df5a
HeyArtem <artem_white@mail.ru>
2020-08-01 02:43:50 +0300

    Теория с примерами. Тернарный опрератор. Оператор Списка, словаря, множества. Задача про имена в нижнем регистре. Лямбда функция, Декораторы. Сравнение время работы генератора и декораторов. Сравнение используемой оперативной памяти итератором и генератором..

--- полигон.py
+++ полигон.py
@@ -138,7 +138,6 @@ def show_time(f):     # --> декоратор
         print(f'{time.time() - t:.5f} сек.')
     return wrapper
 
-
 @show_time  # --> генератор Генерирует список натуральных чисел в пределах num (+1!!!)
 def int_gen_explicit(num = 1000000):
     for i in range(num):
@@ -159,3 +158,55 @@ def time_difference():
 
 
 time_difference()
+
+
+print()
+# Сравнение объема потребляемой оперативной памяти для генератора и списка
+print('    Сравнение объема потребляемой оперативной памяти для генератора и списка')
+
+import os
+import random
+import psutil
+
+''''
+psutil (python system and process utilities) - является кросс-платформенной 
+      библиотекой для получения информации о запущенных процессах 
+      и использования системы (процессор, память, диски, сеть) в Python
+
+Модуль os предоставляет множество функций для работы с операционной системой. 
+     os.getpid() - текущий id процесса.    
+    
+'''
+def show_proc(f):
+    def wrapper(*args, **kwargs):
+        proc = psutil.Process(os.getpid())
+        start = proc.memory_info().rss/1000000
+        f(*args, **kwargs)
+        proc = psutil.Process(os.getpid())
+        stop = proc.memory_info().rss/1000000
+        print(f'{stop-start} Мб.')
+    return wrapper
+
+@show_proc  # генератор
+def int_gen_explicit(num = 1000000):
+    for i in range(num):
+        yield i + 1
+
+@show_proc # итератор
+def int_iter_explicit(num = 1000000):
+    lst = []
+    for i in range(num):
+        lst.append(i+1)
+    return lst
+
+def time_difference():
+    print(f'Объем потребляемой оперативной памяти для генератора составляет:')
+    int_gen_explicit()
+    print(f'Объем потребляемой оперативной памяти для создания списка составляет:')
+    int_iter_explicit()
+time_difference()
+
+
+
+
+
